998dc78aaca0f1a8ef6abda6921e4696304bf06f
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-04-13 23:57:22 +0700

    Create PRACTICE 2.sql

---
+++ PRACTICE 2.sql
@@ -0,0 +1,35 @@
+-- bai tap 1
+select distinct City from Station
+where ID%2=0
+-- bai tap 2
+select count(city) - count(distinct city) from station;
+-- bai tap 3
+-- bai tap 4
+SELECT ROUND(CAST(SUM(item_count * order_occurrences)/SUM(order_occurrences) AS DECIMAL),1) as mean
+FROM items_per_order
+-- bai tap 5
+SELECT candidate_id,
+count(skill) as skill_count
+from candidates
+where skill IN ('Python','Tableau','PostgreSQL')
+GROUP BY candidate_id
+HAVING count(skill) = 3
+--bai tap 6
+SELECT user_id,
+--MIN(post_date::DATE) As first_date,
+--max(post_date::DATE) as last_date,
+max(post_date::DATE) - MIN(post_date::DATE) As days_between
+FROM posts
+WHERE post_date between '01-01-2021' and '12-31-2021'
+GROUP BY user_id
+HAVING count(post_id) > 1 ;
+--bai tap 7
+SELECT 
+card_name,
+--MIN(issued_amount) As min_month,
+--max(issued_amount) as max_month, 
+Max(issued_amount) - MIN(issued_amount) as difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY Max(issued_amount) - MIN(issued_amount) DESC;
+--bai tap 8
