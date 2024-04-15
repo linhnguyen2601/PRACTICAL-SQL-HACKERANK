c8f1ae58013da315ba062a59ee2870b7b8d8e69a
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-04-15 17:13:50 +0700

    Update PRACTICE 3.sql

--- PRACTICE 3.sql
+++ PRACTICE 3.sql
@@ -35,3 +35,23 @@ select
 tweet_id from tweets
 where length(content)>15
 -- bai tap 7
+select 
+activity_date as day,
+count(distinct user_id) as active_users
+from activity
+where (activity_date) between '2019-06-27' and '2019-07-27'
+group by activity_date
+-- bai tap 8
+select
+count (id)
+from employees
+where joining_date between '2022-01-01' and '2022-07-31';
+-- bai tap 9: Find the position of the lower case letter 'a' in the first name of the worker 'Amitah'.'
+select 
+position('a' in first_name) from worker
+where first_name = 'Amitah';
+-- bai tap 10: Find the vintage years of all wines from the country of Macedonia. 
+select 
+title,
+substring(title, length(winery)+ 2,4) as year from winemag_p2
+where country = 'Macedonia';
