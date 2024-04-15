345fceb83d431bbde15bcfff887f33c7c0f2e21e
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-04-15 14:38:31 +0700

    Update PRACTICE 3.sql

--- PRACTICE 3.sql
+++ PRACTICE 3.sql
@@ -22,3 +22,16 @@ FROM reviews
 GROUP BY product_id, extract(month from submit_date)
 ORDER BY extract(month from submit_date), product_id;
 -- bai tap 5: Write a query to identify the top 2 Power Users who sent the highest number of messages on Microsoft Teams in August 2022.
+Select
+sender_id, 
+COUNT(sender_id)
+FROM messages
+where sent_date between '08/01/2022' and '09/01/2022'
+GROUP BY sender_id
+ORDER BY COUNT(message_id) DESC
+limit 2;
+-- bai tap 6
+select 
+tweet_id from tweets
+where length(content)>15
+-- bai tap 7
