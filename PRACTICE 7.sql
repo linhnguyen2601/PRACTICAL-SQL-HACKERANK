da7ccf5a87aa57242b668599cdf23c76ac8bbd86
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-04-26 23:58:52 +0700

    Create PRACTICE 7.sql

---
+++ PRACTICE 7.sql
@@ -0,0 +1,57 @@
+-- cau 1
+With sum_extract AS
+(SELECT product_id, 
+extract(year from transaction_date),
+sum(spend) 
+FROM user_transactions
+group by product_id,extract(year from transaction_date)
+)
+select extract as year, 
+product_id,
+sum as current_year_spend,
+previous_spend,
+round((sum-previous_spend)/previous_spend*100,2) as YOY_growth_rate
+FROM
+(
+select *,
+LAG(sum) OVER(PARTITION by product_id order by extract) as previous_spend
+from sum_extract) as result
+order by product_id,extract
+-- cau 2
+select card_name, issued_amount from (
+SELECT *,
+RANK() OVER(PARTITION BY card_name ORDER BY
+MAKE_DATE(issue_year, issue_month, 1))
+FROM monthly_cards_issued) as a   
+where rank =1
+order by issued_amount DESC
+-- cau 3
+SELECT user_id, spend,transaction_date FROM
+(SELECT *,
+RANK() OVER (PARTITION BY user_id ORDER BY transaction_date) as rank
+FROM transactions) as result
+where rank =3
+-- cau 4
+select transaction_date, user_id, COUNT
+FROM(
+SELECT user_id, count(product_id), transaction_date,
+rank() over(PARTITION BY user_id order by transaction_date DESC)
+FROM user_transactions
+group by user_id, transaction_date) as a  
+where rank =1
+order by transaction_date
+-- cau 5
+-- cau 6
+SELECT count(*) 
+from (
+select transaction_id, 
+EXTRACT(EPOCH FROM transaction_timestamp - lag)/60 as time_span
+FROM (
+SELECT *,
+LAG (transaction_timestamp) 
+OVER(PARTITION BY merchant_id, credit_card_id, amount order by transaction_timestamp)
+FROM transactions) as a
+where EXTRACT(EPOCH FROM transaction_timestamp - lag)/60 <=10) as b  
+-- cau 7
+
+-- cau 8
