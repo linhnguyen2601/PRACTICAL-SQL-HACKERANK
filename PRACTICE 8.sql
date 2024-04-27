4468e42297f086212d18b056fd183d3b6ddc7df6
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-04-28 00:24:18 +0700

    Update PRACTICE 8.sql

--- PRACTICE 8.sql
+++ PRACTICE 8.sql
@@ -40,19 +40,45 @@ LIMIT 10000000
 OFFSET 6
 -- cau 5
 select sum(tiv_2016) as tiv_2016 from Insurance 
-where tiv_2015 in
+where 
+tiv_2015 in
 (select tiv_2015
 from Insurance
 group by tiv_2015
 having count(tiv_2016) >1)
-and (lat, lon) in
+and 
+ (lat, lon) in
 (SELECT lat, lon
 FROM Insurance
 GROUP BY lat, lon
 HAVING COUNT(*) = 1)
 -- cau 6
-
+select b.name as department, a.name as employee, a.salary 
+from (
+select *,
+dense_rank() over(partition by departmentId order by salary desc) as position
+from Employee) as a
+join department as b
+on a.departmentId = b.id
+where position <=3
 -- cau 7
 
 -- cau 8
+with a as (
+select product_id, new_price, change_date,
+rank() over(partition by product_id order by change_date desc) as lan_doi,
+count(change_date) over(partition by product_id) as so_lan_doi,
+lag(new_price) over(partition by product_id order by change_date) as original_price
+from products
+)
+select product_id,
+case 
+when change_date ='2019-08-16' then new_price 
+when (change_date > '2019-08-16' and (max(so_lan_doi) over(partition by product_id)=1)) then 10
+when (change_date > '2019-08-16' and (max(so_lan_doi) over(partition by product_id)>1)) then original_price
+else 0
+end price
+from a
+where lan_doi =1
+
 
