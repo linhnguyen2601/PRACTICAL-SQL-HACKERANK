44d1883048098c8d48c6bc81b411cef4a9ca3794
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-04-27 17:45:26 +0700

    Update PRACTICE 8.sql

--- PRACTICE 8.sql
+++ PRACTICE 8.sql
@@ -26,9 +26,30 @@ round(sum(Case when diff=-1 then 1 else 0 end)/count(distinct(player_id)),2) as 
 from seat
 order by id
 -- cau 4
+with a as
+(select visited_on, sum(amount) as amount from customer
+group by visited_on)
 
+select visited_on,
+sum(amount) over(order by visited_on 
+rows between 6 preceding and current row) as amount,
+round(avg(amount) over(order by visited_on 
+rows between 6 preceding and current row),2) as average_amount
+from a
+LIMIT 10000000
+OFFSET 6
 -- cau 5
-
+select sum(tiv_2016) as tiv_2016 from Insurance 
+where tiv_2015 in
+(select tiv_2015
+from Insurance
+group by tiv_2015
+having count(tiv_2016) >1)
+and (lat, lon) in
+(SELECT lat, lon
+FROM Insurance
+GROUP BY lat, lon
+HAVING COUNT(*) = 1)
 -- cau 6
 
 -- cau 7
