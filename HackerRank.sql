c859f34abde96db71fce83b8f20e64ea0a691a09
linhnguyen2601 <166676829+linhnguyen2601@users.noreply.github.com>
2024-06-06 16:04:05 +0700

    Update HackerRank.sql

--- HackerRank.sql
+++ HackerRank.sql
@@ -11,3 +11,31 @@ case when A + B <= C or A+C <= B or C + B <= A then 'Not A Triangle'
         else 'Scalene'
         end category
 from Triangles;
+
+https://www.hackerrank.com/challenges/the-pads/problem
+Select * from 
+(
+select concat (name, '(', left(occupation, 1), ')') as people from occupations
+order by name asc) as a
+union 
+select * from
+(
+select concat ('There are a total of ', count(occupation), ' ', lower(occupation), 's.') from occupations
+group by occupation
+order by count(occupation), occupation) as b
+
+https://www.hackerrank.com/challenges/occupations/problem        
+with cte as
+(
+select name, occupation,
+rank() over(partition by occupation order by name) as ratingrank from occupations
+order by ratingrank)
+
+select 
+max(case when occupation = 'Doctor' then name else null end) as Doctor,
+max(case when occupation = 'Professor' then name else null end) as Professor,
+max(case when occupation = 'Singer' then name else null end) as Singer,
+max(case when occupation = 'Actor' then name else null end) as Actor
+from cte
+group by ratingrank
+
