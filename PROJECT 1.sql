7b804d4561760b3862dc2912e69bd8deaaca4e48
Nguyen Thuy Linh <166676829+linhnguyen2601@users.noreply.github.com>
2024-07-02 16:36:36 +0700

    Update PROJECT 1.sql

--- PROJECT 1.sql
+++ PROJECT 1.sql
@@ -1,131 +1,129 @@
--- Câu 1)	Chuyển đổi kiểu dữ liệu phù hợp cho các trường ( sử dụng câu lệnh ALTER) 
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN ordernumber TYPE integer USING (trim(ordernumber)::integer)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN quantityordered TYPE integer USING (trim(quantityordered)::integer)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN priceeach TYPE numeric USING (trim(priceeach)::numeric)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN orderlinenumber TYPE integer USING (trim(orderlinenumber)::integer)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN sales TYPE numeric USING (trim(sales)::numeric)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN orderdate TYPE timestamp without time zone USING (trim(orderdate)::timestamp without time zone)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN status TYPE text USING (trim(status)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN productline TYPE text USING (trim(productline)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN addressline1 TYPE text USING (trim(addressline1)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN addressline2 TYPE text USING (trim(addressline2)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN productcode TYPE text USING (trim(productcode)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN customername TYPE text USING (trim(customername)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN phone TYPE text USING (trim(phone)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN city TYPE text USING (trim(city)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN state TYPE text USING (trim(state)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN postalcode TYPE text USING (trim(postalcode)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN country TYPE text USING (trim(country)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN territory TYPE text USING (trim(territory)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN contactfullname TYPE text USING (trim(contactfullname)::text)
-
-ALTER TABLE sales_dataset_rfm_prj
-ALTER COLUMN dealsize TYPE text USING (trim(dealsize)::text)
--- Câu 2)	Check NULL/BLANK (‘’)  ở các trường: ORDERNUMBER, QUANTITYORDERED, PRICEEACH, ORDERLINENUMBER, SALES, ORDERDATE.
-select * from sales_dataset_rfm_prj
-where ordernumber is null 
-
--- Câu 3)	Thêm cột CONTACTLASTNAME, CONTACTFIRSTNAME được tách ra từ CONTACTFULLNAME . 
--- Chuẩn hóa CONTACTLASTNAME, CONTACTFIRSTNAME theo định dạng chữ cái đầu tiên viết hoa, chữ cái tiếp theo viết thường. 
-ALTER TABLE sales_dataset_rfm_prj
-ADD COLUMN CONTACTLASTNAME varchar(50)
-
-ALTER TABLE sales_dataset_rfm_prj
-ADD COLUMN CONTACTFIRSTNAME varchar(50)
-
-UPDATE sales_dataset_rfm_prj
-set CONTACTFIRSTNAME = left(contactfullname, position('-' in contactfullname) -1)
-
-UPDATE sales_dataset_rfm_prj
-set CONTACTLASTNAME = right(contactfullname, length(contactfullname) - position('-' in contactfullname))
-
-ALTER TABLE sales_dataset_rfm_prj
-ADD COLUMN CONTACTLASTNAME1 varchar(50)
-
-ALTER TABLE sales_dataset_rfm_prj
-ADD COLUMN CONTACTFIRSTNAME1 varchar(50)
-
-UPDATE sales_dataset_rfm_prj
-set CONTACTLASTNAME1= CONCAT(upper (left(CONTACTLASTNAME,1)),right(CONTACTLASTNAME,length(CONTACTLASTNAME)-1))
-
-UPDATE sales_dataset_rfm_prj
-set CONTACTFIRSTNAME1= CONCAT(upper(left(CONTACTFIRSTNAME,1)),right(CONTACTFIRSTNAME,length(CONTACTFIRSTNAME)-1))
-
--- Câu 4)	Thêm cột QTR_ID, MONTH_ID, YEAR_ID lần lượt là Qúy, tháng, năm được lấy ra từ ORDERDATE 
-ALTER TABLE sales_dataset_rfm_prj
-ADD COLUMN QTR_ID integer
-
-UPDATE sales_dataset_rfm_prj
-set QTR_ID = extract(quarter from orderdate)
-
-ALTER TABLE sales_dataset_rfm_prj
-ADD COLUMN MONTH_ID integer
-UPDATE sales_dataset_rfm_prj
-set MONTH_ID = extract(month from orderdate)
-
-ALTER TABLE sales_dataset_rfm_prj
-ADD COLUMN YEAR_ID integer
-UPDATE sales_dataset_rfm_prj
-set YEAR_ID = extract(year from orderdate)
--- Câu 5)	Hãy tìm outlier (nếu có) cho cột QUANTITYORDERED và hãy chọn cách xử lý cho bản ghi đó (2 cách) ( Không chạy câu lệnh trước khi bài được review)
-CREATE TABLE boxplot_outlier as
+-- 1. Tạo bảng trong PostgreSQL
+create table SALES_DATASET_RFM_PRJ
 (
-with b as (
-select (Q1 -1.5* IQR) as min_value,
-(Q3 +1.5* IQR) as max_value from
-(select
-percentile_cont(0.25) within group (order by quantityordered) as Q1,
-percentile_cont(0.75) within group (order by quantityordered) as Q3,
-percentile_cont(0.75) within group (order by quantityordered)- percentile_cont(0.25) within group (order by quantityordered) as IQR
-from sales_dataset_rfm_prj) as a
-	)
+  ordernumber VARCHAR,
+  quantityordered VARCHAR,
+  priceeach        VARCHAR,
+  orderlinenumber  VARCHAR,
+  sales            VARCHAR,
+  orderdate        VARCHAR,
+  status           VARCHAR,
+  productline      VARCHAR,
+  msrp             VARCHAR,
+  productcode      VARCHAR,
+  customername     VARCHAR,
+  phone            VARCHAR,
+  addressline1     VARCHAR,
+  addressline2     VARCHAR,
+  city             VARCHAR,
+  state            VARCHAR,
+  postalcode       VARCHAR,
+  country          VARCHAR,
+  territory        VARCHAR,
+  contactfullname  VARCHAR,
+  dealsize         VARCHAR
+) 
+
+-- 2. Đổi kiểu dữ liệu
+ALTER TABLE SALES_DATASET_RFM_PRJ
+ALTER COLUMN ordernumber TYPE integer USING ordernumber::integer,
+ALTER COLUMN quantityordered TYPE integer USING quantityordered::integer,
+ALTER COLUMN priceeach TYPE float USING priceeach::double precision,
+ALTER COLUMN orderlinenumber TYPE integer USING orderlinenumber::integer,
+ALTER COLUMN sales TYPE float USING sales::double precision,
+ALTER COLUMN orderdate TYPE date USING orderdate::date,
+ALTER COLUMN msrp TYPE integer USING msrp::integer
+
+-- 3. Check Null/Blank
+select * from public.sales_dataset_rfm_prj
+where ordernumber is null
+
+select * from public.sales_dataset_rfm_prj
+where quantityordered is null
+
+select * from public.sales_dataset_rfm_prj
+where priceeach is null
+
+select * from public.sales_dataset_rfm_prj
+where orderlinenumber is null
+
+select * from public.sales_dataset_rfm_prj
+where sales is null
+
+select * from public.sales_dataset_rfm_prj
+where orderdate is null
+
+-- 4. Thêm cột CONTACTLASTNAME, CONTACTFIRSTNAME được tách ra từ CONTACTFULLNAME . 
+Chuẩn hóa CONTACTLASTNAME, CONTACTFIRSTNAME theo định dạng chữ cái đầu tiên viết hoa, chữ cái tiếp theo viết thường. 
+
+ALTER TABLE sales_dataset_rfm_prj
+ADD COLUMN first_name varchar,
+ADD COLUMN last_name varchar
+
+UPDATE sales_dataset_rfm_prj
+SET first_name = left(contactfullname, position('-' in contactfullname) -1),
+	last_name = right(contactfullname, length(contactfullname) - position('-' in contactfullname))
+
+ALTER TABLE sales_dataset_rfm_prj
+ADD COLUMN contactfirstname varchar,
+ADD COLUMN contactlastname varchar
+
+UPDATE sales_dataset_rfm_prj
+SET contactfirstname = concat(upper(left(first_name,1)),right(first_name, length(first_name)-1)),
+	contactlastname = concat(upper(left(last_name,1)),right(last_name, length(last_name)-1))
+
+ALTER TABLE sales_dataset_rfm_prj
+DROP first_name,
+DROP last_name
+
+-- 5. Thêm cột QTR_ID, MONTH_ID, YEAR_ID lần lượt là Qúy, tháng, năm được lấy ra từ ORDERDATE 
+ALTER TABLE sales_dataset_rfm_prj
+ADD COLUMN year_id integer,
+ADD COLUMN qtr_id integer,
+ADD COLUMN month_id integer
+
+UPDATE sales_dataset_rfm_prj
+set year_id = extract(year from orderdate),
+	qtr_id = extract(quarter from orderdate),
+	month_id = extract(month from orderdate)
+	
+-- 6. Tìm outlier (nếu có) cho cột QUANTITYORDERED và hãy chọn cách xử lý cho bản ghi đó (2 cách) 
+-- Cách 1: IQR
+with cte as 
+	(
+select percentile_cont(0.25) within group (order by quantityordered) as Q1, 
+	percentile_cont(0.75) within group (order by quantityordered) as Q3, 
+	percentile_cont(0.75) within group (order by quantityordered) - 
+	percentile_cont(0.25) within group (order by quantityordered) as IQR
+	from sales_dataset_rfm_prj),
+cte2 as
+	(
+select Q1 - 1.5*IQR as min_value,
+		Q3 + 1.5*IQR as max_value from cte)
 select quantityordered from sales_dataset_rfm_prj
-where quantityordered< (Select min_value from b) or quantityordered > (select max_value from b)
-	)
+where quantityordered < (select min_value from cte2)
+or quantityordered > (select max_value from cte2)
 
-CREATE TABLE zscore_outlier as 
-(
-with c as (
+-- Cách 2: Z-Score
+with cte as
+	(
 select *,
 (select avg(quantityordered) from sales_dataset_rfm_prj),
 (select stddev(quantityordered) from sales_dataset_rfm_prj)
 from sales_dataset_rfm_prj)
-select quantityordered, (quantityordered-avg)/stddev as z_score from c
-where abs((quantityordered-avg)/stddev)>3)
+
+select quantityordered, (quantityordered - avg)/stddev as z_score from cte
+where abs((quantityordered - avg)/stddev) > 3
+
+--7. Sau khi làm sạch dữ liệu, hãy lưu vào bảng mới tên là SALES_DATASET_RFM_PRJ_CLEAN
+CREATE TABLE sales_dataset_rfm_clean as 
+	
+with cte as
+	(
+select *,
+(select avg(quantityordered) from sales_dataset_rfm_prj),
+(select stddev(quantityordered) from sales_dataset_rfm_prj)
+from sales_dataset_rfm_prj)
+
+select * from cte
+where abs((quantityordered - avg)/stddev) <= 3
